3a19a4603fdb1e613388d33b5dfee2a167d8a774
st186 <subham.tiwari186@gmail.com>
2017-04-16 08:01:11 +0530

    Initial commit

---
+++ app/src/main/java/com/sourcey/materiallogindemo/SignupActivity.java
@@ -0,0 +1,216 @@
+package com.sourcey.materiallogindemo;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sourcey.materiallogindemo.LoginActivity;
+import com.sourcey.materiallogindemo.R;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import butterknife.ButterKnife;
+import butterknife.Bind;
+
+public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+
+    @Bind(R.id.input_name) EditText _nameText;
+    @Bind(R.id.input_address) EditText _addressText;
+    @Bind(R.id.input_email) EditText _emailText;
+    @Bind(R.id.input_mobile) EditText _mobileText;
+    @Bind(R.id.input_password) EditText _passwordText;
+    @Bind(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
+    @Bind(R.id.btn_signup) Button _signupButton;
+    @Bind(R.id.link_login) TextView _loginLink;
+
+    private static final String REGISTER_URL="https://www.commissarial-primit.000webhostapp.com/public_html/UserRegistration/register.php";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+        }
+
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating Account...");
+        progressDialog.show();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+        finish();
+    }
+
+    public void onSignupFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String address = _addressText.getText().toString();
+        String email = _emailText.getText().toString();
+        String mobile = _mobileText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 3) {
+            _nameText.setError("at least 3 characters");
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (address.isEmpty()) {
+            _addressText.setError("Enter Valid Address");
+            valid = false;
+        } else {
+            _addressText.setError(null);
+        }
+
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError("enter a valid email address");
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (mobile.isEmpty() || mobile.length()!=10) {
+            _mobileText.setError("Enter Valid Mobile Number");
+            valid = false;
+        } else {
+            _mobileText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+            _reEnterPasswordText.setError("Password Do not match");
+            valid = false;
+        } else {
+            _reEnterPasswordText.setError(null);
+        }
+
+        return valid;
+    }
+    private void registerUser(){
+        String name =_nameText.getText().toString().trim().toLowerCase();
+        String address =_addressText.getText().toString().trim().toLowerCase();
+        String email =_emailText.getText().toString().trim().toLowerCase();
+        String mobile = _mobileText.getText().toString().trim().toLowerCase();
+        String password =_passwordText.getText().toString().trim().toLowerCase();
+        String confirmpassword=_reEnterPasswordText.getText().toString().trim().toLowerCase();
+        register(name,address,email,mobile,password,confirmpassword);
+    }
+    public void register(String name,String address,String email,String mobile,String password,String confirmpassword){
+        String urlsuffix="?name="+name+"?address="+address+"?email="+email+"?mobile="+mobile+"?password="+password+"?confirmpassword="+confirmpassword;
+        class registerUser extends AsyncTask<String,Void,String> {
+            ProgressDialog loading;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(SignupActivity.this, "Please Wait", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                String s = params[0];
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(REGISTER_URL + s);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String result;
+                    result = bufferedReader.readLine();
+                    return result;
+
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        registerUser ur=new registerUser();
+        ur.execute(urlsuffix);
+
+
+                }
+            }
+
